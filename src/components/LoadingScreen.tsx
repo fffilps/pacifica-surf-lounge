3a00b60a34aaa9b3559d146cf63c67543f83b744
fffilps <jmariwyatt@gmail.com>
2024-08-01 16:38:08 -0700

    fixed typescript build errors on loadingscreen regarding unused items

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const LoadingScreen = () => {
-  const [visible, setVisible] = useState(true);
 
 const [open, setOpen] = useState(true)
 
