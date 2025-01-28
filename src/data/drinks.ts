618aef0d8470c826eda9f8083660b19a9bd20fa4
fffilps <jmariwyatt@gmail.com>
2025-01-28 14:57:15 -0800

    updated the prices on all of the drinks. increased them all by $1

--- src/data/drinks.ts
+++ src/data/drinks.ts
@@ -4,42 +4,42 @@ export const drinks = [
       {
         name: "Surf Lounge's Ice Tea",
         subtitle: "Our Specialty!",
-        price: "$22.00",
+        price: "$23.00",
       },
       {
         name: "Passion Punch",
         subtitle: "Packs A Sweet Punch",
-        price: "$22.00",
+        price: "$23.00",
       },
       {
         name: "Hawaiian Volcano",
         subtitle: "You'll Be On Fire After This",
-        price: "$22.00",
+        price: "$23.00",
       },
       {
         name: "P-38",
         subtitle: "Strikes Like Lightning",
-        price: "$22.00",
+        price: "$23.00",
       },
       {
         name: "Scorpion (Serves Four)",
         subtitle: "Watch Out, This One Strings",
-        price: "$20.00",
+        price: "$21.00",
       },
       {
         name: "Lover's Cup",
         subtitle: "Served Lovingly For Two",
-        price: "$20.00",
+        price: "$21.00",
       },
       {
         name: "Mangareva",
         subtitle: "You'll All Be Blended",
-        price: "$20.00",
+        price: "$21.00",
       },
       {
         name: "Tidal Wave",
         subtitle: "This One Will Wipe You Out",
-        price: "$20.00",
+        price: "$21.00",
       },
     ],
   },
@@ -48,142 +48,142 @@ export const drinks = [
       {
         name: "Zombie",
         subtitle: "After Two, Your Dead To Us",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Bahama Mama",
         subtitle: "Light, Dark, Black and Juicy",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Navy Grog",
         subtitle: "You'll Feel Like You're At Sea",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Tonga",
         subtitle: "Like A Bahama Mama With A Bat",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "June Bug",
         subtitle: "Watch Out, It's Got A Bite",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Cap'n Bill's Grog",
         subtitle: "This Will Get You Shipwrecked",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Coco Loco",
         subtitle: "This One May Make You Nutty",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Pina Colada",
         subtitle: "A Chilling Experience",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Lava Flow",
         subtitle: "Tasted Like Melted Hawaii",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Yellow Bird",
         subtitle: "You'll Be Flying",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Mango Mai-Tai",
         subtitle: "A Mango Delight",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Duke's Special",
         subtitle: "Nothing's As Smooth As The Duke",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Red Hibiscus",
         subtitle: "You'll Be Looking For Love",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Strawberry or Banana Daiquiris Sangria",
         subtitle: "A Refreshing Fruity Wine, MMMMM",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Hurricane",
         subtitle: "Don't Get Blown Away",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Planter's Punch",
         subtitle: "This Is The Pay-off",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Wind Surfer",
         subtitle: "You'll Hit The Rocks",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "South Sea Daiquiri",
         subtitle: "Hits Something Like A Typhoon",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Mai-Tai",
         subtitle: "The Only Way To Tai One On",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Banana Cow",
         subtitle: "This Is The Crazy Side",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Island Gold",
         subtitle: "A Real Tropical Treasure",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Typhoon",
         subtitle: "You'll Be Spinning",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Wipe-Out",
         subtitle: "You'll Know The Surf Is Up",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Sadie Thompson",
         subtitle: "A Tropical Volcano",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Tahitian Deep Purple",
         subtitle: "Makes You See Rainbows",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Sabotage",
         subtitle: "Go Ahead, Mess Yourself Up",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Hawaiian Sunset",
         subtitle: "Makes You Feel Romantic",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Diablo",
         subtitle: "Named For The Devil So It Has To Be Good",
-        price: "$9.00",
+        price: "$10.00",
       },
     ],
   },
@@ -192,37 +192,37 @@ export const drinks = [
       {
         name: "Chi-Chi",
         subtitle: "Nice Coconuts Make This Special",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Vodka Vapor",
         subtitle: "Get Vaporized",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Surf Lounge's Lemonade",
         subtitle: "Not The Kiddy Kind",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Fuji Explosion",
         subtitle: "An Explosive Apple Delight",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Blue Hawaii",
         subtitle: "Like The Ocean On A Calm Day",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Beachcomber Ken's Treat",
         subtitle: "Makes You Feel Frisky",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Moscow Mule",
         subtitle: "Kicks You In The Rear",
-        price: "$9.00",
+        price: "$10.00",
       },
     ],
   },
@@ -231,52 +231,52 @@ export const drinks = [
       {
         name: "King Louie",
         subtitle: "A Tribute To Our Patriarch",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Tahiti Monk",
         subtitle: "Get Your Brother Blended",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Colorado Blended Bulldog",
         subtitle: "Good Doggy",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Toasted Almond",
         subtitle: "A Smooth And Nutty Pleasure",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Butter Scotch Surprise",
         subtitle: "A Candied Treat",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Chocolate Banana",
         subtitle: "You'll Go Ape For One",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Banana Spilt",
         subtitle: "Without The Boat Of Course",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Grasshopper",
         subtitle: "Take One From My Hand",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Pineapple Freeze",
         subtitle: "Taste Like A Hawaiian Wiinter",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Orange Freeze",
         subtitle: "Just Like An Orange Cream Sickle",
-        price: "$10.00",
+        price: "$11.00",
       },
     ],
   },
@@ -285,92 +285,92 @@ export const drinks = [
       {
         name: "Mickey Feather Stone",
         subtitle: "You'll Plead Insanity Afterwards",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Black Magic",
         subtitle: `The "Lover's" Potion`,
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "New Orlean's Fog Cutter",
         subtitle: "You dont Need To See Clear In The Fog",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Monkey's Tail",
         subtitle: "You'll Be Hanging From The Rafters",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Tropical Itch",
         subtitle: "Scratch The Surface With A Straw",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Surfer On Acid",
         subtitle: "Mavericks On The Rocks",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Long Island Ice Tea",
         subtitle: "Ice Tea For Grown Ups",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Hawaiian Tea",
         subtitle: "Tropical Tea For Grown Ups",
-        price: "$10.00",
+        price: "$11.00",
       },
       {
         name: "Lynchburg Lemonade",
         subtitle: "Lemonade For Grown Ups",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Mediterranean Splash",
         subtitle: "This Is A Natural",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Tropical Virgin",
         subtitle: "The Delight Of The Tropics",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Singapore Sling",
         subtitle: "In Shanghai, The Law Is Three",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Rastaman",
         subtitle: "Say Mon, Dis One's A Killah",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Mango Madness",
         subtitle: "This Is One Crazy Drink",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Catamaran",
         subtitle: "Fast and Smooth",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Sweet Lei Lani",
         subtitle: "The Hawaiian Sweetheart",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Pisco Punch",
         subtitle: "It Will Smack you Right In The Head",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Shark's Tooth",
         subtitle: "Two and You'll Be Biting The Sharks",
-        price: "$9.00",
+        price: "$10.00",
       },
     ],
   },
@@ -379,26 +379,26 @@ export const drinks = [
       {
         name: "Tequila Sunrise",
         subtitle: "A Mexican Delight",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Ice Breaker",
         subtitle: "It Will Loosen Up Most",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Strawberry or Banana Margarita",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Beyond The Reef",
         subtitle: "Add Two For Indepth Conversation",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Texas Punch",
         subtitle: "Stir It Up Texas Style",
-        price: "$9.00",
+        price: "$10.00",
       },
     ],
   },
@@ -407,32 +407,32 @@ export const drinks = [
       {
         name: "Keoke Coffee",
         subtitle: "Wake up Island Style",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Mexican Coffee",
         subtitle: "Wake Up South of The Border",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Irish Coffee",
         subtitle: "It Helps When You're Feeling Green",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Spanish Coffee",
         subtitle: "It's Pretty Good, No Bull",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Hot Spiced Buttered Rum",
         subtitle: "An American Classic",
-        price: "$7.00",
+        price: "$8.00",
       },
       {
         name: "Hot Toddy",
         subtitle: "Makes You Feel All Warn Inside",
-        price: "$6.00",
+        price: "$7.00",
       },
     ],
   },
@@ -440,67 +440,67 @@ export const drinks = [
     shotsMartinis: [
       {
         name: "Irish Car Bomb",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Sour Apple Martini",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "B-52",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Red Headed Slut",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Liquid Cocaine",
-        price: "$9.00",
+        price: "$10.00",
       },
       {
         name: "Watermelon Jolly Rancher",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Chocolate Martini",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "French Martini",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Cosmopolitan",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Lemon Drop",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Brain Tumor",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Kamikaze",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Scooby Snack",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Oatmeal Cookie",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Chocolate Cake",
-        price: "$8.00",
+        price: "$9.00",
       },
       {
         name: "Peanut Butter & Jelly",
-        price: "$8.00",
+        price: "$9.00",
       },
     ],
   },
@@ -508,19 +508,19 @@ export const drinks = [
     houseDrinks: [
       {
         name: "Well Drinks",
-        price: "$5.00"
+        price: "$6.00"
       },
       {
         name: "Draft Beer",
-        price: "$6.00"
+        price: "$7.00"
       },
       {
         name: "Champagne Split",
-        price: "$6.00"
+        price: "$7.00"
       },
       {
         name: "House Wine",
-        price: "$6.00"
+        price: "$7.00"
       }
     ]
   }
